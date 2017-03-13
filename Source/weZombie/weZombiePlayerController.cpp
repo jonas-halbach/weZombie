bd56272588c35f5a44925d5197a5dc0893ff0540
jonas-halbach <jonas@jonas-halbach.de>
2017-03-14 00:40:35 +0100

    default player controller now handles different mouse states

--- Source/weZombie/weZombiePlayerController.cpp
+++ Source/weZombie/weZombiePlayerController.cpp
@@ -14,6 +14,10 @@ AweZombiePlayerController::AweZombiePlayerController()
 {
 	bShowMouseCursor = true;
 	DefaultMouseCursor = EMouseCursor::Crosshairs;
+
+	playerControllerStatus = PlayerControllerStatus();
+	selectedPawns = TArray<APawn *>();
+
 	UE_LOG(LogClass, Warning, TEXT("Create PlayerController"));
 }
 
@@ -22,6 +26,8 @@ void AweZombiePlayerController::SetupInputComponent()
 {
 	// set up gameplay key bindings
 	Super::SetupInputComponent();
+
+	LocalPlayer = Cast<ULocalPlayer>(Player);
 
 	InputComponent->BindAction("SetDestination", EInputEvent::IE_Pressed, this, &AweZombiePlayerController::OnMouseDown);
 	InputComponent->BindAction("SetDestination", EInputEvent::IE_Released, this, &AweZombiePlayerController::OnMouseUp);
@@ -33,9 +39,16 @@ void AweZombiePlayerController::SetupInputComponent()
 
 void AweZombiePlayerController::OnMouseDown()
 {
-	/*FVector MousePos;
-	FVector2D MousePosition;
-	FVector MouseDir;
+	/////
+	///// Something like the following goes here:
+	///// AHUD.GetActorsInSelectionRectangle
+	/////
+	///// maybe check:  https://www.youtube.com/watch?v=yCqsbXS9yRg
+	///// and: https://wiki.unrealengine.com/HUD,_Canvas,_Code_Sample_of_800%2B_Lines,_Create_Buttons_%26_Draw_Materials
+	/////
+	
+	//FVector MousePos;
+	/*FVector MouseDir;
 
 	float x;
 	float y;
@@ -47,6 +60,12 @@ void AweZombiePlayerController::OnMouseDown()
 		this->SetActorLocation(MousePos, false);
 	}
 	*/
+
+
+	FVector2D mousePos;
+	GetCurrentMousePosition(mousePos);
+	playerControllerStatus.MouseDown(mousePos);
+
 	FHitResult Hit;
 	GetHitResultUnderCursor(ECC_Visibility, false, Hit);
 
@@ -66,9 +85,16 @@ void AweZombiePlayerController::OnMouseDown()
 
 }
 
+void AweZombiePlayerController::OnMouseMove()
+{
+	FVector2D mousePos;
+	GetCurrentMousePosition(mousePos);
+	playerControllerStatus.MouseMove(mousePos);
+}
+
 void AweZombiePlayerController::OnMouseUp()
 {
-
+	playerControllerStatus.MouseUp();
 }
 
 void AweZombiePlayerController::OnTouchDown(const ETouchIndex::Type FingerIndex, const FVector Location)
@@ -78,16 +104,57 @@ void AweZombiePlayerController::OnTouchDown(const ETouchIndex::Type FingerIndex,
 
 void AweZombiePlayerController::SetNewMoveDestination(const FVector DestLocation)
 {
-	APawn* const Pawn = GetPawn();
-	if (Pawn)
+	for(int i = 0; i < selectedPawns.Num(); i++)
 	{
-		UNavigationSystem* const NavSys = GetWorld()->GetNavigationSystem();
-		float const Distance = FVector::Dist(DestLocation, Pawn->GetActorLocation());
-
-		// We need to issue move command only if far enough in order for walk animation to play correctly
-		if (NavSys && (Distance > 120.0f))
+		TArray<UActorComponent*> characterMovement;
+		selectedPawns[i]->GetComponents(characterMovement);
+		if (characterMovement.Num() > 0)
 		{
-			NavSys->SimpleMoveToLocation(this, DestLocation);
+			///!!!!Attention!!!! This will not work this way, because playercontroller just controls one pawn!!!!!!
+			UNavigationSystem* const NavSys = GetWorld()->GetNavigationSystem();
+			float const Distance = FVector::Dist(DestLocation, selectedPawns[i]->GetActorLocation());
+
+			// We need to issue move command only if far enough in order for walk animation to play correctly
+			if (NavSys && (Distance > 120.0f))
+			{
+				NavSys->SimpleMoveToLocation(this, DestLocation);
+			}
 		}
 	}
+}
+
+bool AweZombiePlayerController::IsMouseDown()
+{
+	return playerControllerStatus.IsMouseDown();
+}
+
+void AweZombiePlayerController::ProcessPlayerInput(float deltaTime, bool gamePaused)
+{
+	Super::ProcessPlayerInput(deltaTime, gamePaused);
+	if (!gamePaused)
+	{
+		OnMouseMove();
+	}
+}
+
+void AweZombiePlayerController::GetSelectionRect(FIntRect &selectionRect)
+{
+	playerControllerStatus.GetMouseRect(selectionRect);
+}
+
+void AweZombiePlayerController::GetCurrentMousePosition(FVector2D &mousePos)
+{
+	if (LocalPlayer && LocalPlayer->ViewportClient)
+	{
+		FVector2D MousePosition;
+		if (LocalPlayer->ViewportClient->GetMousePosition(MousePosition))
+		{
+			mousePos = MousePosition;
+		}
+	}
+}
+
+void AweZombiePlayerController::AddCurrentSelection(TArray<APawn *> &currentSelection)
+{
+	selectedPawns = currentSelection;
 }
--- Source/weZombie/weZombiePlayerController.h
+++ Source/weZombie/weZombiePlayerController.h
@@ -1,6 +1,8 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 #pragma once
 #include "GameFramework/PlayerController.h"
+#include "PlayerControllerStatus.h"
+#include "Runtime/Engine/Classes/Engine/LocalPlayer.h"
 #include "weZombiePlayerController.generated.h"
 
 UCLASS()
@@ -10,14 +12,25 @@ class AweZombiePlayerController : public APlayerController
 
 public:
 	AweZombiePlayerController();
+	bool IsMouseDown();
+	void GetSelectionRect(FIntRect &selectionRect);
+
+	void AddCurrentSelection(TArray<APawn *> &currentSelectedPawns);
 
 protected:
 	void SetupInputComponent();
 	void OnMouseDown();
 	void OnMouseUp();
+	void OnMouseMove();
 	void OnTouchDown(const ETouchIndex::Type FingerIndex, const FVector Location);
 	void SetNewMoveDestination(const FVector DestLocation);
+	void GetCurrentMousePosition(FVector2D &mousePos);
+	virtual void ProcessPlayerInput(float deltaTime, bool isPaused) override;
 
+	PlayerControllerStatus playerControllerStatus;
+	ULocalPlayer* LocalPlayer;
+
+	TArray<APawn *> selectedPawns;
 };
 
 
