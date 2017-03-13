754e03080eec29213c794eddfb7ed93efd0136ca
jonas-halbach <jonas@jonas-halbach.de>
2017-03-14 00:36:50 +0100

    added AIModule to project modules

--- Source/weZombie/weZombie.Build.cs
+++ Source/weZombie/weZombie.Build.cs
@@ -6,6 +6,6 @@ public class weZombie : ModuleRules
 {
 	public weZombie(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule" });
 	}
 }
